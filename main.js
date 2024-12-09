1241604a41043421ee3f3195f65675df04a146e2
yung-saintDev <vovalyubkin@gmail.com>
2024-12-09 23:01:25 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+let count = document.querySelector(".points-int")
+let circle = document.querySelector(".circle-wrapper")
+let zeroNum = 0
+
+circle.addEventListener("click", function(){
+    zeroNum++
+
+    count.innerHTML = zeroNum
+})
