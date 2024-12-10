4210a861dd41c730838cfb541853cd362b30d06a
yung-saintDev <vovalyubkin@gmail.com>
2024-12-10 20:21:26 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
 
 
-        document.getElementById("nextButton").onclick = function() {
-            var userName = document.getElementById("inputval").value;
-            if (userName) {
-                window.location.href = "main.html?name=" + encodeURIComponent(userName);
-            } else {
-                alert("Please enter a name!");
-            }
-        };
+
+document.getElementById('inputval').addEventListener('keypress', function(e) {
+    if (e.key === 'Enter') {
+        e.preventDefault(); 
+        document.getElementById('nameForm').submit(); 
+    }
+});
+
+document.getElementById('nameForm').onsubmit = function(event) {
+    event.preventDefault(); 
+    var userName = document.getElementById('inputval').value;
+    if (userName) {
+        window.location.href = '/main.html?name=' + encodeURIComponent(userName);
+    } else {
+        alert("Please enter a name!");
+    }
+};
