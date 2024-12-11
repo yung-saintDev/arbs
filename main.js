826fd2696a21769f15cc923f32609b2d158864a6
yung-saintDev <vovalyubkin@gmail.com>
2024-12-11 16:02:40 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,6 @@
 let count = document.querySelector(".points-int");
 let circle = document.querySelector(".circle-wrapper");
 let zeroNum = 0;
-const urlParams = new URLSearchParams(window.location.search);
-const userName = urlParams.get('name'); 
-
-if (userName) {
-    document.getElementById('userName').textContent = userName;
-}
 
 circle.addEventListener("click", function(){
     zeroNum++;
